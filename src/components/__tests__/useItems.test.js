74956c0368e183b13ead8f089df59f4085486817
Huseyin Altikulaç <huseyinaltikulac4@gmail.com>
2021-08-13 12:21:02 +0300

    Test Success

--- src/components/__tests__/useItems.test.js
+++ src/components/__tests__/useItems.test.js
@@ -2,14 +2,16 @@ import { expect, test } from "@jest/globals";
 import { useItems } from "../hooks/useItems";
 import { renderHook } from "@testing-library/react-hooks";
 import { Provider } from "react-redux";
-import { store } from "../../store"
+import { store } from "../../store";
+import { addItem } from "../../store/items/actions";
 
 const wrapper = ({ children }) => (
     <Provider store={store}>{children}</Provider>
 )
 test("gives an state items", async () => {
-    const { result } = renderHook(() => useItems(), { wrapper });
-    const { items, addItem } = result.current;
+    const { result } = renderHook(() => useItems({ addItem }), { wrapper });
+    const { items, bindActions } = result.current;
     expect(items).toHaveLength(1);
-    expect(addItem).toBeInstanceOf(Function);
+    expect(bindActions).toBeInstanceOf(Object);
+    expect(bindActions.addItem).toBeInstanceOf(Function);
 })
